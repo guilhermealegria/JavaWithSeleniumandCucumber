a356b9d9521725162d470fdc01c0bd36bd18dbfa
Guilherme Alegria <guilherme.alegria@primeup.com.br>
2023-08-05 15:04:59 -0300

    Add dependencies and test selenium

--- TemplateForJava/src/test/java/org/example/AppTest.java
+++ TemplateForJava/src/test/java/org/example/AppTest.java
@@ -1,13 +1,26 @@
 package org.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AppTest {
-
+    public static WebDriver driver = null;
     private App app = new App();
-   @Test
     public void TesteAdicao(){
        Assertions.assertEquals(2,app.adicao(1,1));
+   }
+
+   @Test
+    public void TesteNavegarUrl(){
+       WebDriverManager.chromedriver().setup();
+       driver = new ChromeDriver();
+       driver.get("https://www.google.com/");
+       Assertions.assertEquals("https://www.google.com/", driver.getCurrentUrl());
+       driver.close();
+
    }
 }
